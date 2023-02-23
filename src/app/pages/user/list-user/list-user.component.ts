98c36f1995cc3047077db73014cc2d4b6667b311
Daniel <daniel.uremix@gmail.com>
2023-02-23 03:07:00 -0400

    User list

--- src/app/pages/user/list-user/list-user.component.ts
+++ src/app/pages/user/list-user/list-user.component.ts
@@ -1,13 +1,26 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {SupabaseService} from "../../../supabase.service";
 
 @Component({
   selector: 'app-list-user',
   templateUrl: './list-user.component.html',
   styleUrls: ['./list-user.component.css']
 })
-export class ListUserComponent {
+export class ListUserComponent implements OnInit{
   dataSet: any;
 
-  constructor() {
+  constructor(private supabaseService: SupabaseService) {
+    this.dataSet = [];
+  }
+
+  ngOnInit(): void {
+    this.supabaseService.getUsers().then(({data, error}) => {
+      let dataValue: any = data;
+      if (error) {
+        console.log('error', error)
+      }else{
+        this.dataSet = dataValue;
+      }
+    });
   }
 }
--- src/app/supabase.service.ts
+++ src/app/supabase.service.ts
@@ -82,4 +82,9 @@ export class SupabaseService {
     const {data, error} = await this.supabase.auth.signUp({email, password});
     return {data, error};
   }
+
+  async getUsers() {
+    const {data, error} = await this.supabase.from('user_list').select('*');
+    return {data, error};
+  }
 }
